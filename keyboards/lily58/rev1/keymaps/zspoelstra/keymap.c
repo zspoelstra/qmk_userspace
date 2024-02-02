af9805d8c2bb908e11e3b7032bc9c2537cdcb4ea
Zach Spoelstra <zspoelstra@gmail.com>
2024-02-02 12:31:40 -0500

    Disable update_tri_layer_state

--- keyboards/lily58/rev1/keymaps/zspoelstra/keymap.c
+++ keyboards/lily58/rev1/keymaps/zspoelstra/keymap.c
@@ -74,9 +74,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 )
 };
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE);
-}
+// layer_state_t layer_state_set_user(layer_state_t state) {
+//   return update_tri_layer_state(state, _LOWER, _RAISE);
+// }
 
 //SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
 #ifdef OLED_ENABLE
