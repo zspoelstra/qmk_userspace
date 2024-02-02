53d6756149a497ea006a517dd9ce811f692b7a2a
Zach Spoelstra <zspoelstra@gmail.com>
2024-02-02 12:35:05 -0500

    Home row mods

--- keyboards/lily58/rev1/keymaps/zspoelstra/config.h
+++ keyboards/lily58/rev1/keymaps/zspoelstra/config.h
@@ -23,3 +23,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_LEFT
 #define QUICK_TAP_TERM 0
 #define TAPPING_TERM 200
+
+// Left-hand home row mods
+#define HOME_A LCTL_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LGUI_T(KC_D)
+#define HOME_F LSFT_T(KC_F)
+
+// Right-hand home row mods
+#define HOME_J RSFT_T(KC_J)
+#define HOME_K RGUI_T(KC_K)
+#define HOME_L LALT_T(KC_L)
+#define HOME_SCLN RCTL_T(KC_SCLN)
