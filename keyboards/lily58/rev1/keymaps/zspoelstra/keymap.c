f00913039c2fea8405be0163069b770071217c5b
Zach Spoelstra <zspoelstra@gmail.com>
2024-03-24 09:13:12 -0400

    Swap space and backspace

--- keyboards/lily58/rev1/keymaps/zspoelstra/keymap.c
+++ keyboards/lily58/rev1/keymaps/zspoelstra/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |      |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |      |      |      | /BackSP /       \Space \  |      |      |      |
+ *                   |      |      |      | /Space  /       \Backsp\  |      |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,      KC_MINS,
   XXXXXXX,  HOME_A, HOME_S,  HOME_D,  HOME_F,  KC_G,                     KC_H,    HOME_J,  HOME_K,  HOME_L,  HOME_SCLN, KC_QUOT,
   XXXXXXX,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX,  XXXXXXX,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   XXXXXXX,
-                       XXXXXXX, XXXXXXX, XXXXXXX, KC_BSPC,  KC_SPC, XXXXXXX, XXXXXXX, XXXXXXX
+                       XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  KC_BSPC, XXXXXXX, XXXXXXX, XXXXXXX
 )
 };
 
