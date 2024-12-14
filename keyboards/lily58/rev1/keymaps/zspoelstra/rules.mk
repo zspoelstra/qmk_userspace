e543ce8f47d2eb55ddfd576e134763e14389c670
Zach Spoelstra <zspoelstra@gmail.com>
2024-12-14 12:37:19 -0500

    Sort rules. Add BOOTLOADER

--- keyboards/lily58/rev1/keymaps/zspoelstra/rules.mk
+++ keyboards/lily58/rev1/keymaps/zspoelstra/rules.mk
@@ -1,15 +1,16 @@
-LTO_ENABLE = yes            # Link Time Optimization enabled
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = no        # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no            # N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+BOOTLOADER = atmel-dfu
+COMMAND_ENABLE = no         # Commands for debug and configuration
+CONSOLE_ENABLE = no         # Console for debug
+EXTRAKEY_ENABLE = no        # Audio control and System control
+LTO_ENABLE = yes            # Link Time Optimization enabled
+MOUSEKEY_ENABLE = no        # Mouse keys
+NKRO_ENABLE = no            # N-Key Rollover
+OLED_ENABLE = yes           # OLED display
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
-OLED_ENABLE = yes           # OLED display
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
